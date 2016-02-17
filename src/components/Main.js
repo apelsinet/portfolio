b7fdad1fba9dd04a8c29e824825edebec5938bc2
apelsinet <mattiasprada@gmail.com>
2016-02-18 00:17:22 +0100

    Add margin to headings.

--- src/components/Main.js
+++ src/components/Main.js
@@ -33,9 +33,9 @@ const Main = (props) => {
         <h2>Heading</h2>
         <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
         <blockquote>
-          <p>“Two things are infinite: the universe and human stupidity; and I am not sure about the universe.”</p>
-          <cite>—Albert Einstein</cite>
+        “Two things are infinite: the universe and human stupidity; and I am not sure about the universe.”
         </blockquote>
+        <cite>—Albert Einstein</cite>
       </section>
       <section>
         <h3>Heading</h3>
