dd1ef017b1b8c10a745c5d62db3a832771231c16
apelsinet <mattiasprada@gmail.com>
2016-03-01 15:56:05 +0100

    Remove orion image.

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,11 +3,7 @@ import React, {PropTypes} from 'react';
 const Header = (props) => {
   return (
     <div>
-      <div className="bar">
-      </div>
-      <div className="header-image">
-        <h1>Mattias Prada</h1>
-      </div>
+      <div className="bar"></div>
     </div>
   );
 };
