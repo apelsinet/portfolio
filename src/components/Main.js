ba39bc517fa8d8250446c587d975fa173822fe22
apelsinet <mattiasprada@gmail.com>
2016-03-13 18:45:26 +0100

    Remove react-markdown in favor of escaped jsx.

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,36 +1,5 @@
 import React, {PropTypes} from 'react';
-let ReactMarkdown = require('react-markdown');
 
-let input = [
-  '# Live demo\n\nChanges are automatically rendered as you type.\n\n* Follows the ',
-  '[CommonMark](http://commonmark.org/) spec\n* Renders actual, "native" React DOM ',
-  'elements\n* Allows you to escape or skip HTML (try toggling the checkboxes above)',
-  '\n* If you escape or skip the HTML, no `dangerouslySetInnerHTML` is used! Yay!\n',
-  '\n## HTML block below\n\n<blockquote>\n    This blockquote will change based ',
-  'on the HTML settings above.\n</blockquote>\n\n## How about some code?\n',
-  '```js\nvar React = require(\'react\');\nvar Markdown = require(\'react-markdown\');',
-  '\n\nReact.render(\n    <Markdown source="# Your markdown here" />,\n    document.',
-  'getElementById(\'content\')\n);\n```\n\nPretty neat, eh?\n\n', '## More info?\n\n',
-  'Read usage information and more on [GitHub](//github.com/rexxars/react-markdown)\n\n',
-  '---------------\n\n',
-  'A component by [VaffelNinja](http://vaffel.ninja) / Espen Hovlandsdal\n\n',
-  '```html\n',
-  '<html>\n',
-  '  <head>\n',
-  '    <title></title>\n',
-  '    <link rel="stylesheet" href="somedomain.com/style.css"/>\n',
-  '  </head>\n',
-  '</html>\n',
-  '```\n',
-  'Some c++\n',
-  '```c++\n',
-  '#include <iostream>\n',
-  'int main() {\n',
-  '  std::cout << "Hellow World!";\n',
-  '  //commented text goes here\n',
-  '}\n',
-  '```\n'
-].join('');
 
 const Main = (props) => {
   return (
@@ -54,6 +23,30 @@ const Main = (props) => {
       </section>
       <section>
         <h2>Heading</h2>
+        <p>Now to some code snippets. Let's start with the css.</p>
+        <pre><code className="css">{`body { color: blue; }`}</code></pre>
+        <p>Add a touch of html.</p>
+        <pre><code className="html">{`<html>
+  <head>
+    <title></title>
+    <link rel='stylesheet' href='somedomain.com/style.css'/>
+  </head>
+</html>`}</code></pre>
+        <p>Here comes the javascript</p>
+        <pre><code className="javascript">{`var React = require('react');
+var Markdown = require('react-markdown');
+
+React.render(
+    <Markdown source="# Your markdown here" />,
+    document.getElementById('content')
+);`}</code></pre>
+        <p>Finally, onwards with some c++</p>
+        <pre><code className="c++">{`#include <iostream>
+int main() {
+  std::cout << "Hellow World!";
+  //commented text goes here
+}`}</code></pre>
+
         <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
         <blockquote>
         “Two things are infinite: the universe and human stupidity; and I am not sure about the universe.”
@@ -67,9 +60,6 @@ const Main = (props) => {
       <section>
         <h4>Heading</h4>
         <p>Some text...</p>
-      </section>
-      <section>
-        <ReactMarkdown source={input} />
       </section>
     </main>
   );
