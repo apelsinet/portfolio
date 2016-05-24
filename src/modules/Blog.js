333c36764dc6b616a85f2badd6486f0e26fa8820
apelsinet <mattiasprada@gmail.com>
2016-05-25 01:02:08 +0200

    Redo blog snippets mockup.

--- src/modules/Blog.js
+++ src/modules/Blog.js
@@ -27,23 +27,32 @@ export default class Blog extends React.Component {
 					? <Coffee closeCoffeeTip={this._closeCoffeeTip.bind(this)} />
 					: ''
 			}
-			<h2>Third blog post</h2>
-			<p className="publication-date">Published 2016-04-28.</p>
-			<p> Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
-			<a href="">READ MORE</a>
 			</section>
-			<section>
-			<h2>Second blog post</h2>
-			<p className="publication-date">Published 2016-04-25</p>
-			<p> Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
-			<a href="">READ MORE</a>
+			<section className="blog-snippet">
+				<a href="">
+					<h2>Third blog post</h2>
+					<p><i>2016-04-28</i><span className="blog-tag">Latin</span><span className="blog-tag">Jibberish</span></p>
+					<p> Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+					<p><i>Read more&hellip;</i></p>
+				</a>
 			</section>
-			<section>
-			<h2>First blog post</h2>
-			<p className="publication-date">Published 2016-04-22</p>
-			<p> Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
-			<a href="">READ MORE</a>
+			<section className="blog-snippet">
+				<a href="">
+					<h2>Second blog post</h2>
+					<p><i>2016-04-26</i><span className="blog-tag">Latin</span><span className="blog-tag">Jibberish</span></p>
+					<p> Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+					<p><i>Read more&hellip;</i></p>
+				</a>
 			</section>
+			<section className="blog-snippet">
+				<a href="">
+					<h2>First blog post</h2>
+					<p><i>2016-04-25</i><span className="blog-tag">Latin</span><span className="blog-tag">Jibberish</span></p>
+					<p> Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+					<p><i>Read more&hellip;</i></p>
+				</a>
+			</section>
+			
 			</div>
 		);
 	}
