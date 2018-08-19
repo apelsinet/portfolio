d1805ebaa6033aa4935a865a98f257a3506b15c6
apelsinet <mattiasprada@gmail.com>
2018-08-19 13:30:32 +0200

    Remove public from repo

--- public/path----52c17507cfcadd5130ff.js
+++
@@ -1,2 +0,0 @@
-webpackJsonp([60335399758886],{104:function(t,e){t.exports={data:{site:{siteMetadata:{title:"apelsi.net"}}},layoutContext:{}}}});
-//# sourceMappingURL=path----52c17507cfcadd5130ff.js.map
--- public/path---orange-a0e39f21c11f6a62c5ab.js
+++
@@ -1,2 +0,0 @@
-webpackJsonp([98232277910121],{323:function(t,n){t.exports={pathContext:{}}}});
-//# sourceMappingURL=path---orange-a0e39f21c11f6a62c5ab.js.map
