56fe7f5cfb23aaba6d02ed489a00d747bdc41ba2
apelsinet <mattiasprada@gmail.com>
2018-10-27 00:37:46 +0200

    Half of grid done

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -2,38 +2,27 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import Helmet from 'react-helmet';
 
-import Header from '../components/Header';
+// import Header from '../components/Header';
 import './index.css';
 
 const Layout = ({ children, data }) => (
-    <div
-        style={{
-            alignItems: 'center',
-            background: '#c02942',
-            display: 'flex',
-            flexDirection: 'column',
-            minHeight: '100vh',
-        }}
-    >
+    <div>
         <Helmet
             title={data.site.siteMetadata.title}
             meta={[
                 { name: 'description', content: 'Portfolio of Mattias Prada' },
-                { name: 'keywords', content: 'mattias prada, mattias, prada, full stack developer, portfolio' },
-                { name: 'google-site-verification', content: 'wmG8XkUWPbLucXGWpHwSpH6RsTwGXX3SAPyHl_cxrCg' },
+                {
+                    name: 'keywords',
+                    content:
+                        'mattias prada, mattias, prada, full stack developer, portfolio',
+                },
+                {
+                    name: 'google-site-verification',
+                    content: 'wmG8XkUWPbLucXGWpHwSpH6RsTwGXX3SAPyHl_cxrCg',
+                },
             ]}
         />
-        <div
-            style={{
-                display: 'flex',
-                flexDirection: 'column',
-                flex: 1,
-                maxWidth: 960,
-                width: '100%',
-            }}
-        >
-            {children()}
-        </div>
+        {children()}
     </div>
 );
 
--- src/pages/index.js
+++ src/pages/index.js
@@ -2,66 +2,77 @@ import React, { Component } from 'react';
 import Link from 'gatsby-link';
 import Orange from './Orange';
 
+const AboutSection = () => (
+    <div>
+        <p>
+            Lorem Ipsum is simply dummy text of the printing and typesetting
+            industry. Lorem Ipsum has been the industry's standard dummy text
+            ever since the 1500s, when an unknown printer took a galley of type
+            and scrambled it to make a type specimen book. It has survived not
+            only five centuries, but also the leap into electronic typesetting,
+            remaining essentially unchanged. It was popularised in the 1960s
+            with the release of Letraset sheets containing Lorem Ipsum passages,
+            and more recently with desktop publishing software like Aldus
+            PageMaker including versions of Lorem Ipsum.
+        </p>
+    </div>
+);
+
 class IndexPage extends Component {
     state = {
-        isOrangeFloored: false,
+        isOrangeRoofed: false,
     };
 
     onOrangeClick = () =>
-        this.setState({ isOrangeFloored: !this.state.isOrangeFloored });
+        this.setState({ isOrangeRoofed: !this.state.isOrangeRoofed });
 
     render() {
-        const { isOrangeFloored } = this.state;
+        const { isOrangeRoofed } = this.state;
+        // 4rem per column
         return (
-            <div
-                style={{
-                    alignItems: 'center',
-                    display: 'flex',
-                    flex: 1,
-                    justifyContent: 'center',
-                    overflow: 'hidden',
-                }}
-            >
-                <header
-                    style={{
-                        opacity: isOrangeFloored ? 1 : 0,
-                        position: 'absolute',
-                        textAlign: 'center',
-                        transform: isOrangeFloored
-                            ? 'translateY(0)'
-                            : 'translateY(3vh)',
-                        transition:
-                            'opacity 0.3s ease-in, transform 0.2s ease-in',
-                    }}
-                >
-                    <h1 style={{ marginBottom: 0 }}>
-                        <a
-                            href="https://www.linkedin.com/in/mattias-prada-2aa2a743/"
-                            target="_blank"
-                        >
-                            Mattias Prada
-                        </a>
-                    </h1>
-                    <p style={{ textTransform: 'uppercase' }}>
-                        Full stack developer
-                    </p>
-                </header>
+            <div className={`grid ${!isOrangeRoofed ? 'fade-out' : ''}`}>
+                <div className="orange-container">
+                    <Orange
+                        alt="Orange"
+                        onClick={this.onOrangeClick}
+                        style={{
+                            cursor: 'pointer',
+                            transform: isOrangeRoofed
+                                ? 'translateY(-25vh)'
+                                : 'translateY(25vh)',
+                            transition:
+                                'transform 0.2s ease-in-out, filter 0.1s ease-out',
+                            width: '100%',
+                        }}
+                    />
+                </div>
+                <h1 className="serif masthead">
+                    Mattias Prada
+                </h1>
+                <img src={require('./lang-icons.svg')} className="lang-icons"/>
+                <h2 className="sans full-stack">Full stack developer</h2>
+                <div className="serif copy-container welcome">
+                    <p>We had entered the clouds.</p>
+                    <p>For half-an-hour we were muffled in a cold, damp mist, and total darkness, and had begun to think of going indoors when, all at once, the car burst into the pure and starlit region of the upper air.</p>
+                    <p>A cry of joyous admiration escaped from us all.</p>
+                    <p>"How beautiful!" exclaimed Miss Carmichael, in a solemn whisper, as if she were afraid that angels might hear. "The atmosphere, too, is often very impure," I replied, also in a whisper. "I say, Miss Carmichael! Won't you come and take a look at Venus?" Then I whispered into space.</p>
+                    <p>"There is Venus right ahead," cried the astronomer, but in a softer tone than usual, perhaps out of respect for the sovereign laws of the universe. "The course is clear now--we are fairly on the open sea--I mean the open ether. I must get out my telescope."</p>
+                    <p>The spectacle before us was indeed sublime.</p>
+                </div>
+                <p className="sans side welcome">1. Welcome</p>
+                <div className="serif copy-container about">
+                    <p>The sky of a deep dark blue was hung with innumerable stars, which seemed to float in the limpid ether, and the rolling vapours through which we had passed were drawn like a sable curtain between us and the lower world. The stillness was so profound that we could hear the beating of our own hearts.</p>
+                    <p>"How beautiful!" exclaimed Miss Carmichael, in a solemn whisper, as if she were afraid that angels might hear. "The atmosphere, too, is often very impure," I replied, also in a whisper. "I say, Miss Carmichael! Won't you come and take a look at Venus?" Then I whispered into space.</p>
+                    <p>"There is Venus right ahead," cried the astronomer, but in a softer tone than usual, perhaps out of respect for the sovereign laws of the universe. "The course is clear now--we are fairly on the open sea--I mean the open ether. I must get out my telescope."</p>
+                </div>
+                <p className="sans side about">2. About</p>
+                <div className="space bg"/>
+                <img src={require('./starfield.svg')} className="space starfield"/>
+                <div className="serif copy-container space">
+                    <p>The sky of a deep dark blue was hung with innumerable stars, which seemed to float in the limpid ether, and the rolling vapours through which we had passed were drawn like a sable curtain between us and the lower world. The stillness was so profound that we could hear the beating of our own hearts.</p>
+                </div>
+                <p className="sans side space">3. Space</p>
 
-                <Orange
-                    alt="Orange"
-                    onClick={this.onOrangeClick}
-                    style={{
-						cursor: 'pointer',
-                        margin: 0,
-                        maxWidth: '400px',
-                        transform: isOrangeFloored
-                            ? 'translateY(50vh)'
-                            : 'translateY(0)',
-                        transition:
-                            'transform 0.2s ease-in-out, filter 0.1s ease-out',
-                        width: '70vw',
-                    }}
-                />
             </div>
         );
     }
