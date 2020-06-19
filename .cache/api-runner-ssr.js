a8354b74211445e01db82502f5cd4080835421d3
apelsinet <mattiasprada@gmail.com>
2020-06-19 20:17:25 +0200

    Make a red redesign

--- .cache/api-runner-ssr.js
+++ .cache/api-runner-ssr.js
@@ -2,8 +2,11 @@ var plugins = [{
       plugin: require('/Users/apelsinet/h/portfolio/node_modules/gatsby-plugin-react-helmet/gatsby-ssr'),
       options: {"plugins":[]},
     },{
+      plugin: require('/Users/apelsinet/h/portfolio/node_modules/gatsby-plugin-manifest/gatsby-ssr'),
+      options: {"plugins":[],"name":"mattias-prada-portfolio","short_name":"portfolio","start_url":"/","background_color":"#ed1b24","theme_color":"#ed1b24","display":"minimal-ui","icon":"src/images/favicon.png","cache_busting_mode":"query","include_favicon":true,"legacy":true,"theme_color_in_head":true,"cacheDigest":"520be3186fdc43c3bc344b609dc5ab59"},
+    },{
       plugin: require('/Users/apelsinet/h/portfolio/node_modules/gatsby-plugin-google-fonts/gatsby-ssr'),
-      options: {"plugins":[],"fonts":["Open Sans","Markazi Text"]},
+      options: {"plugins":[],"fonts":["Inter:400,700"],"display":"swap"},
     }]
 // During bootstrap, we write requires at top of this file which looks like:
 // var plugins = [
@@ -20,17 +23,22 @@ var plugins = [{
 const apis = require(`./api-ssr-docs`)
 
 // Run the specified API in any plugins that have implemented it
-module.exports = (api, args, defaultReturn) => {
+module.exports = (api, args, defaultReturn, argTransform) => {
   if (!apis[api]) {
     console.log(`This API doesn't exist`, api)
   }
 
   // Run each plugin in series.
+  // eslint-disable-next-line no-undef
   let results = plugins.map(plugin => {
-    if (plugin.plugin[api]) {
-      const result = plugin.plugin[api](args, plugin.options)
-      return result
+    if (!plugin.plugin[api]) {
+      return undefined
     }
+    const result = plugin.plugin[api](args, plugin.options)
+    if (result && argTransform) {
+      args = argTransform({ args, result })
+    }
+    return result
   })
 
   // Filter out undefined results.
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,70 +1,67 @@
-import React, { Component } from 'react';
-import Link from 'gatsby-link';
-import Orange from './Orange';
+import React from "react"
 
-class IndexPage extends Component {
-    state = {
-        isOrangeFloored: false,
-    };
+import Layout from "../components/layout"
+import SEO from "../components/seo"
 
-    onOrangeClick = () =>
-        this.setState({ isOrangeFloored: !this.state.isOrangeFloored });
-
-    render() {
-        const { isOrangeFloored } = this.state;
-        return (
-            <div
-                style={{
-                    alignItems: 'center',
-                    display: 'flex',
-                    flex: 1,
-                    justifyContent: 'center',
-                    overflow: 'hidden',
-                }}
-            >
-                <header
-                    style={{
-                        opacity: isOrangeFloored ? 1 : 0,
-                        position: 'absolute',
-                        textAlign: 'center',
-                        transform: isOrangeFloored
-                            ? 'translateY(0)'
-                            : 'translateY(3vh)',
-                        transition:
-                            'opacity 0.3s ease-in, transform 0.2s ease-in',
-                    }}
-                >
-                    <h1 style={{ marginBottom: 0 }}>
-                        <a
-                            href="https://www.linkedin.com/in/mattias-prada-2aa2a743/"
-                            target="_blank"
-                        >
-                            Mattias Prada
-                        </a>
-                    </h1>
-                    <p style={{ textTransform: 'uppercase' }}>
-                        Full stack developer
-                    </p>
-                </header>
-
-                <Orange
-                    alt="Orange"
-                    onClick={this.onOrangeClick}
-                    style={{
-						cursor: 'pointer',
-                        margin: 0,
-                        maxWidth: '400px',
-                        transform: isOrangeFloored
-                            ? 'translateY(50vh)'
-                            : 'translateY(0)',
-                        transition:
-                            'transform 0.2s ease-in-out, filter 0.1s ease-out',
-                        width: '70vw',
-                    }}
-                />
-            </div>
-        );
-    }
+function yearsDiff() {
+  const date1 = new Date()
+  const date2 = new Date("2017-02-15")
+  const yearsDiff = Math.abs(date2.getFullYear() - date1.getFullYear())
+  return yearsDiff < 10 ? `0${yearsDiff}` : yearsDiff
 }
 
-export default IndexPage;
+const IndexPage = () => (
+  <Layout>
+    <SEO title="Portfolio | Mattias Prada" />
+    <h2 className="keywords">
+      Leadership, product focus and code craftmanship
+    </h2>
+    <h1 className="name">Mattias Prada</h1>
+    <div className="line top"></div>
+    <div className="role">Engineering Manager from Stockholm, Sweden</div>
+    <p className="copy first">
+      I believe anyone can build fantastic products given the opportunity, if
+      they have a passion, the right support and are eager to learn.
+    </p>
+    <p className="copy second">
+      Today I work at Epidemic Sound, where I support an amazing team of highly
+      skilled Fullstack Software Engineers, and occasionally also get my hands
+      dirty with code, which I love.
+    </p>
+    <p className="copy third">
+      We build products that helps our end users discover new music on our
+      platform; through search, recommendations and curation.
+    </p>
+    <p className="copy fourth">
+      I thrive when I get to learn new things, and discuss topics close to my
+      heart like functional programming, system architecture, web performance
+      and design.
+    </p>
+    <div className="line bottom"></div>
+    <div className="squares left">
+      <div className="square white"></div>
+      <div className="square white"></div>
+      <div className="square white"></div>
+    </div>
+    <div className="squares right">
+      <div className="square black"></div>
+      <div className="square black"></div>
+    </div>
+    <p className="copy fifth">
+      I have a background in sound and broadcast engineering, but started my
+      professional journey into software engineering in 2017.
+    </p>
+    <p className="copy sixth">
+      For leisure I enjoy spending time with my son and partner, as well as
+      playing video games, reading, watching rocket launches and learning new
+      things.
+    </p>
+    <div className="number">{yearsDiff()}</div>
+    <div className="links">
+      <a href="https://www.linkedin.com/in/mattias-prada-2aa2a743/">Linkedin</a>
+      <a href="https://github.com/apelsinet">GitHub</a>
+    </div>
+  </Layout>
+)
+
+export default IndexPage
